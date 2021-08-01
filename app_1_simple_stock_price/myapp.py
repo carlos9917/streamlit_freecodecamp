b72cd94ef940343185522d6ce47ccc9e93280738
Carlos Peralta <carlos9917@gmail.com>
2021-08-01 15:31:26 +0200

    Moved some dirs around

--- app_1_simple_stock_price/myapp.py
+++ app_1_simple_stock_price/myapp.py
@@ -15,6 +15,8 @@ tickerSymbol = 'GOOGL'
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+import pdb
+pdb.set_trace()
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDf.Close)
